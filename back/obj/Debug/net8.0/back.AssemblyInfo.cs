f13b30ca87ab6474fcd9fa0b5f888e9cd1d94355
nasazz <anasazzouz2002@gmail.com>
2024-09-12 18:41:41 +0100

    ANAS COMMIT

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da4a812f944e0af0942dfa326685d0f911102a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bf42b20b502fba3c99157e71f66a0d29347d015")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
