10cf3c96b08d3e8c78027f8627e2a2dd9cd6ca30
Houssine-Kh <houssine.khafif@gmail.com>
2024-09-28 22:42:07 +0100

    configuration add bug

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9aefcc88d3e9eb6951237c472fb408f9f482d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb18f63bbed7de097c69b46226f9135dfa1847b")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
